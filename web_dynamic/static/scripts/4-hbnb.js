5095533a3c2f62288183b2b5e2f665c25813ed80
mattowsh <mattobelen@gmail.com>
2022-12-23 23:08:58 -0300

    task 5. duplicates problem unsolved, multiple amenities problem unsolved, 0 amenities results: fixed

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -58,40 +58,9 @@ window.onload = function () {
       }
     })
 
-  // task 4.
-  // Request http://0.0.0.0:5001/api/v1/places_search/
-  // Now, places are loaded from the front-end, not from the back-end
-  // const section = document.querySelector('.places');
-  // fetch('http://localhost:5001/api/v1/places_search', {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json'
-  //   },
-  //   body: JSON.stringify({})
-  // })
-  //   .then(response => response.json())
-  //   .then(data => {
-  //     for (place of data) {
-  //       section.innerHTML += `
-  //       <article>
-  //           <div class="title_box">
-  //             <h2>${place.name}</h2>
-  //             <div class="price_by_night">$${place.price_by_night}</div>
-  //           </div>
-  //           <div class="information">
-  //             <div class="max_guest">${place.max_guest} Guest${place.max_guest != 1 ? "s" : ""}</div>
-  //                   <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms != 1 ? "s" : ""}</div>
-  //                   <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms != 1 ? "s" : ""}</div>
-  //           </div>
-  //                 <div class="description">
-  //             ${place.description}
-  //                 </div>
-  //         </article>
-  //       `;
-  //     }
-  //   })
-
-
+    // task 4.
+    placesFromFrontend();
+    
     // task 5. 
     // Implement a filter:
     // When the button tag is clicked, a new POST request to places_search
@@ -109,6 +78,10 @@ window.onload = function () {
       })
       .then(response => response.json())
       .then(data => {
+        if (amenitiesID.length === 0) {
+          placesFromFrontend();
+          return;
+        }
         const placesSection = document. querySelector('.places');
         for (place of data) {
           for (id of place.amenity_ids) {
@@ -135,4 +108,39 @@ window.onload = function () {
         }
       }
     )}
+};
+
+function placesFromFrontend () {
+  // task 4.
+  // Request http://0.0.0.0:5001/api/v1/places_search/
+  // Now, places are loaded from the front-end, not from the back-end
+  const section = document.querySelector('.places');
+  fetch('http://localhost:5001/api/v1/places_search', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({})
+  })
+    .then(response => response.json())
+    .then(data => {
+      for (place of data) {
+        section.innerHTML += `
+        <article>
+            <div class="title_box">
+              <h2>${place.name}</h2>
+              <div class="price_by_night">$${place.price_by_night}</div>
+            </div>
+            <div class="information">
+              <div class="max_guest">${place.max_guest} Guest${place.max_guest != 1 ? "s" : ""}</div>
+                    <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms != 1 ? "s" : ""}</div>
+                    <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms != 1 ? "s" : ""}</div>
+            </div>
+                  <div class="description">
+              ${place.description}
+                  </div>
+          </article>
+        `;
+      }
+    })
 };
